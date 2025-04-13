86bf6304c517621a033b5a484d00e5658f3a8d89
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-04-13 18:00:24 +0530

    feat: added comment file

---
+++ Day2/comments.java
@@ -0,0 +1,8 @@
+public class comments {
+    public static void main(String[] args) {
+        System.out.println("Single line comment");//this symbol(//) used for single line comment
+        System.out.println("Multi line comment");//this (/* */) is used for multi line comment
+
+    }
+    
+}
