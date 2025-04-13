058a29909fdfb2a62acc396f5db6ebb8008b77c2
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-04-13 19:07:20 +0530

    feat: added datatype file

---
+++ Day2/datatype.java
@@ -0,0 +1,12 @@
+
+public class datatype {
+    public static void main(String[] args) {
+        int myNum = 5; //primitive data type ( int, float, char, boolean)
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello"; //non-primitive data type
+        System.out.println(myNum + "\n" +myFloatNum + "\n" + myLetter + "\n" + myBool + "\n" + myText);
+    }
+    
+}
