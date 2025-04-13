331c42f2be3e84921ff0d58ab1ae3eb32fb99e59
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-04-13 17:52:13 +0530

    feat: added print formats

---
+++ Day2/print.java
@@ -0,0 +1,6 @@
+public class print {
+    public static void main(String[] args) {
+        System.out.println("This is the way to print text"); //inside double quote for text
+        System.out.println(10000); // no quotes for numbers
+        System.out.println(342814183581325f);
+}
