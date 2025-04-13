150e242e871781bfc81ce88489d55629e22ba844
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-04-13 18:44:40 +0530

    feat: added taski  file

---
+++ Day2/taski.java
@@ -0,0 +1,10 @@
+public class taski {
+    public static void main(String[] args) {
+        double area,pi,r;
+        r = 5.6;
+        pi = 3.1416;
+        area = pi * r * r;
+        System.out.println("Area of circle is " + area); 
+    }
+    
+}
