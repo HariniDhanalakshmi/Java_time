cf0188b01bced3f17a6294ac13543e9c768b4a86
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-04-13 18:36:45 +0530

    feat: added variables  file

---
+++ Day2/variables.java
@@ -0,0 +1,21 @@
+public class variables {
+    public static void main(String[] args) {
+        int myNum = 5; //declaration using variables
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+        String firstname = "Computer";
+        String lastname = "Science";
+        int d,e,f;
+        d = e = f = 33;
+        final int g = 45; // this value is constant 
+        int a = 3, b = 6, c = 67; //Multiple declaration
+        System.out.println(myNum + " " +myFloatNum + " " + myLetter + " " + myBool + " " + myText);
+        System.out.println(myNum+myFloatNum); //operation
+        System.out.println(a+b+c); 
+        System.out.println(firstname+lastname);//concatenate
+        System.out.println(d);
+        System.out.println(g);
+    }
+}
