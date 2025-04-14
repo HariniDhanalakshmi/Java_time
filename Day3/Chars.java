b3906ceea7ccdf42bc73bf83751357c37566a626
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-04-14 12:36:53 +0530

    Feat:Char program added

---
+++ Day3/Chars.java
@@ -0,0 +1,7 @@
+public class Chars{
+    public static void main(String[] args) {
+      char a = 'X';
+      System.out.println("a contains " +a++);
+      System.out.println("a now contains " +a);  
+    }
+}
