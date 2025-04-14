c018490d5f40b6b7cae439dd90a8371b2b0b103e
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-04-14 12:30:32 +0530

    Feat:area program added

---
+++ Day3/area.java
@@ -0,0 +1,10 @@
+public class area {
+    public static void main(String[] args) {
+        float length, breadth;
+        double area;
+        length = 43.5f;
+        breadth = 89.34f;
+        area = 0.5 * length * breadth;
+        System.out.println("Area of triangle: " + area);
+    }
+}
