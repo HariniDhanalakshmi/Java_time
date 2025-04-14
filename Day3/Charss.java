cd3a137bc56459389b074d82cf1fd8e77dadd4ec
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-04-14 13:17:01 +0530

    Ascii program added

---
+++ Day3/Charss.java
@@ -0,0 +1,7 @@
+public class Charss {
+    public static void main(String[] args) {
+        char ch1 = 88, ch2 = 'Y';
+        System.out.println("ch1 and ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
